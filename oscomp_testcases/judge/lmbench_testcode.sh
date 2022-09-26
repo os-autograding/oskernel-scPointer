85cfe500fa72e2872493bbd9f05b4c8c669fd20e
yfblock <321353225@qq.com>
2022-09-26 15:51:24 +0800

    Update lmbench_testcode.sh

--- oscomp_testcases/judge/lmbench_testcode.sh
+++ oscomp_testcases/judge/lmbench_testcode.sh
@@ -25,13 +25,13 @@ busybox echo file system latency
 lmbench_all lat_fs /var/tmp
 busybox echo Bandwidth measurements
 lmbench_all bw_pipe -P 1
-echo -n "lmbench_all bw_file_rd -P 1 512k io_only /var/tmp/XXX: "
+echo -n "bw_file_rd -P 1 512k io_only /var/tmp/XXX: "
 lmbench_all bw_file_rd -P 1 512k io_only /var/tmp/XXX
-echo -n "lmbench_all bw_file_rd -P 1 512k open2close /var/tmp/XXX: "
+echo -n "bw_file_rd -P 1 512k open2close /var/tmp/XXX: "
 lmbench_all bw_file_rd -P 1 512k open2close /var/tmp/XXX
-echo -n "lmbench_all bw_mmap_rd -P 1 512k mmap_only /var/tmp/XXX: "
+echo -n "bw_mmap_rd -P 1 512k mmap_only /var/tmp/XXX: "
 lmbench_all bw_mmap_rd -P 1 512k mmap_only /var/tmp/XXX
-echo -n "lmbench_all bw_mmap_rd -P 1 512k open2close /var/tmp/XXX: "
+echo -n "bw_mmap_rd -P 1 512k open2close /var/tmp/XXX: "
 lmbench_all bw_mmap_rd -P 1 512k open2close /var/tmp/XXX
 busybox echo context switch overhead
 lmbench_all lat_ctx -P 1 -s 32 2 4 8 16 24 32 64 96
